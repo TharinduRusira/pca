4cd424c9084b15c943c207b603dae0c0f6e90559
Tharindu  Kumara <tkumara@Mac-mini-Tharindu.local>
2013-12-30 13:20:15 +0530

    renamed preprocessor methods

--- tests/PCADriverTest.java
+++ tests/PCADriverTest.java
@@ -12,25 +12,25 @@ import core.PCAPreProcessor;
 public class PCADriverTest extends PCADriver {
   
   @Test
-  public void meanNormalizeTest() {
+  public void zeroMeanNormalizeTest() {
     DenseMatrix input = new DenseMatrix(new double[][] { {1, 2, 3}, {100, 500, 2}, {300, 6, 40}});
     DenseMatrix expected = new DenseMatrix(new double[][] {
         {-132.66666666666666, -167.33333333333334, -12.0},
         {-33.66666666666666, 330.66666666666663, -13.0},
         {166.33333333333334, -163.33333333333334, 25.0}});
     PCAPreProcessor prepro = new PCAPreProcessor();
-    DenseMatrix result = prepro.meanNormalize(input);
+    DenseMatrix result = prepro.zeroMeanNormalize(input);
     assertEquals("meanNormalizeTest failed", expected.asFormatString(), result.asFormatString());
   }
   
   @Test
-  public void featureScaleTest() {
+  public void unitVarianveFeatureScaleTest() {
     DenseMatrix input = new DenseMatrix(new double[][] { {1, 2, 3}, {4, 5, 6}});
     DenseMatrix expected = new DenseMatrix(new double[][] {
         {0.34299717028501764, 0.5252257314388902, 0.6324555320336759},
         {1.3719886811400706, 1.3130643285972254, 1.2649110640673518}});
     PCAPreProcessor prepro = new PCAPreProcessor();
-    DenseMatrix result = prepro.featureScale(input);
+    DenseMatrix result = prepro.unitVarianceFeatureScale(input);
     assertEquals("featureScaleTest failed", expected.asFormatString(), result.asFormatString());
     
   }
