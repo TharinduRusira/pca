c39ffb6348df60ebb2c21081126d1dbdf06237ce
Tharindu Rusira <tharindurusira@gmail.com>
2013-12-31 10:59:55 +0530

    code formatted

--- core/AbstractPCAEngine.java
+++ core/AbstractPCAEngine.java
@@ -1,17 +1,18 @@
 package core;
 
 import org.apache.mahout.math.DenseMatrix;
-/** 
+
+/**
  * 
- * Input data matrix is supposed to be of the following format.
- * Rows --> Data records
- * Columns --> features 
- *  
+ * Input data matrix is supposed to be of the following format. Rows --> Data records Columns --> features
+ * 
  */
 
 public interface AbstractPCAEngine {
   public DenseMatrix runPCA();
+  
   public DenseMatrix getCovarianceMatrix();
+  
   public DenseMatrix getEigenVectors();
   
 }
