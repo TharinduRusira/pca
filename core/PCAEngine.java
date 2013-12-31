a53a5639c2b291a95412b8306d7f4a86a6f5e3fe
Tharindu Rusira <tharindurusira@gmail.com>
2013-12-31 11:00:56 +0530

    covariance method implemented

--- core/PCAEngine.java
+++ core/PCAEngine.java
@@ -1,18 +1,20 @@
 package core;
 
 import org.apache.mahout.math.DenseMatrix;
+import org.apache.mahout.math.Matrix;
 
-public class PCAEngine implements AbstractPCAEngine{
-  DenseMatrix input,reducedMatrix;
+public class PCAEngine implements AbstractPCAEngine {
+  DenseMatrix input, reducedMatrix;
   int inputSize;
   int features;
   int reducedSize;
   
-  public PCAEngine(DenseMatrix in){
-	  this.input = in;
-	  this.inputSize = in.rowSize();
-	  this.features = in.columnSize();
-	  this.reducedSize = features; //default values set as the same numbers of original features
+  public PCAEngine(DenseMatrix in) {
+    this.input = in;
+    this.inputSize = in.rowSize();
+    this.features = in.columnSize();
+    // default value is set as the same numbers as the number of original features
+    this.reducedSize = features;
   }
   
   public PCAEngine(DenseMatrix in, int k) {
@@ -21,40 +23,43 @@ public class PCAEngine implements AbstractPCAEngine{
     this.features = in.columnSize();
     this.reducedSize = k;
   }
+  
   /**
    * 
    * @return
    */
   @Override
-  public DenseMatrix runPCA(){
+  public DenseMatrix runPCA() {
     reducedMatrix = new DenseMatrix(inputSize, reducedSize);
     // implement pca here
     return reducedMatrix;
   }
+  
   /**
    * 
    * @return
    */
   @Override
-  public DenseMatrix getCovarianceMatrix(){
-    DenseMatrix covar = new DenseMatrix(inputSize, features);
-    // calculate covariance matrix
-    DenseMatrix rowi;
-    for(int i=0;i<this.inputSize;i++){
-    	//read data records line by line
-    	rowi = (DenseMatrix)input.viewPart(i, 1,0,features);
-    	covar.assign(covar.plus(rowi.transpose().times(rowi)));
+  public DenseMatrix getCovarianceMatrix() {
+    DenseMatrix covar = new DenseMatrix(features, features);
+    // calculate Covariance matrix
+    Matrix rowi;
+    for (int i = 0; i < this.inputSize; i++) {
+      rowi = input.viewPart(i, 1, 0, features);
+      covar.assign(covar.plus(rowi.transpose().times(rowi)));
     }
     covar.assign(covar.divide(inputSize));
     return covar;
   }
+  
   /**
-   * Implements Eigen Vectors using Single Value Decomposition(SVD) method 
-   * @return
+   * Calculates Eigen Vectors of a given Co-variance matrix using Single Value Decomposition(SVD) method
+   * 
+   * @return u
    */
   @Override
   public DenseMatrix getEigenVectors() {
-    // TODO Auto-generated method stub
+    
     return null;
   }
 }
