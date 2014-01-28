667644bf9ae841a5ce3b3398f6636d1858cef8cf
Tharindu Rusira <tharindurusira@gmail.com>
2014-01-29 05:24:21 +0530

    added a test body. Implement it

--- tests/PCAEngineTest.java
+++ tests/PCAEngineTest.java
@@ -10,10 +10,15 @@ import org.junit.Test;
 import core.PCAEngine;
 
 public class PCAEngineTest {
+  DenseMatrix input;
   
+  @BeforeClass
+  public void init(){
+	    input = new DenseMatrix(new double[][] { {1, 2, 3}, {4, 5, 6}});
+
+  }
   @Test
   public void testPCAEngineConstructor2(){
-    DenseMatrix input = new DenseMatrix(new double[][] { {1, 2, 3}, {4, 5, 6}});
     try{
       // this call should generate an IllegalArgumentException
       new PCAEngine(input, 5);
@@ -27,14 +32,12 @@ public class PCAEngineTest {
   
   @Test
   public void testCovarianceMatrix() {
-    DenseMatrix input = new DenseMatrix(new double[][] { {1, 2, 3}, {4, 5, 6}});
     DenseMatrix expected = new DenseMatrix(new double[][] { {8.5, 11.0, 13.5}, {11.0, 14.5, 18.0}, {13.5, 18.0, 22.5}});
     DenseMatrix result = new PCAEngine(input).getCovarianceMatrix();
     assertEquals("Covariance Matrix Test failed", expected.asFormatString(), result.asFormatString());
   }
   @Test
   public void testCovarianceMatrixVectorized(){
-    DenseMatrix input = new DenseMatrix(new double[][] { {1, 2, 3}, {4, 5, 6}});
     DenseMatrix expected = new DenseMatrix(new double[][] { {8.5, 11.0, 13.5}, {11.0, 14.5, 18.0}, {13.5, 18.0, 22.5}});
     DenseMatrix result = new PCAEngine(input).getCovarianceMatrixVectorized();
     assertEquals("Covariance Matrix Test failed", expected.asFormatString(), result.asFormatString());
@@ -43,6 +46,11 @@ public class PCAEngineTest {
   @Test
   public void testGetEigenVectors(){
     
+  }
+  
+  @Test
+  public void getKPrincipalComponentsTest(){
+	  
   }
   
 }
